a285de6d416e441904f287f45782f85273832002
Tuan Nguyen <tuanna.it96@gmail.com>
2021-01-02 15:33:35 +0700

    editshowpost

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,1 +1,57 @@
-show.blade.php
+@extends('user.layout.layout')
+@section('content')
+<head>
+	<style type="text/css">
+		img{
+			max-width: 90%;
+			display: block;
+            margin: auto;
+            margin-top: 5px;
+            margin-bottom: 5px;
+		}
+		.single_page > h1 {
+		    color: #333;
+		    font-family: 'Cuprum', sans-serif;
+		    font-size: 30px;
+		    line-height: 38px;
+		    margin: 10px 0 -10px;
+		    padding: 0 0 4px;
+		    text-transform: uppercase;
+		    font-weight: 600;
+		}
+		
+	</style>
+</head>
+<section class="col-lg-8 col-md-8 col-sm-8">
+        <div class="left_content">
+          <div class="single_page">
+            <ol class="breadcrumb">
+              <li><a href="../index.html">Home</a></li>
+              <li><a href="#">Technology</a></li>
+              <li class="active">Mobile</li>
+            </ol>
+            <h1>{{$post->title}}</h1>
+
+            <div class="post_commentbox"> <a href="#"><i class="fa fa-user"></i>Wpfreeware</a> <span><i class="fa fa-calendar"></i>6:49 AM</span> <a href="#"><i class="fa fa-tags"></i>Technology</a> </div>
+           
+            <div class="single_page_content">
+            @if($post->thumbimg =! null)
+            <img style="width: 500px; height: auto;" class="img-center" src="{{$post->thumbimg}}" alt="">
+            @endif
+              <div class="divpost" >{!!$post->content!!}</div>
+            </div>
+           <!--  <div class="social_link">
+              <ul class="sociallink_nav">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+              </ul>
+            </div> -->
+            
+          </div>
+        </div>
+
+      </section>
+@endsection
--- resources/views/user/layout/slide.blade.php
+++ resources/views/user/layout/slide.blade.php
@@ -1,28 +1,12 @@
 <div class="col-lg-8 col-md-8 col-sm-8">
         <div class="slick_slider">
-          <div class="single_iteam"> <a href="pages/single_page.html"> <img src="{{asset('user_asset/assets/images/logo.jpg')}}" alt=""></a>
+          @foreach($postslide as $post)
+          <div class="single_iteam"> <a href="pages/single_page.html"> <img src="{{$post->thumbimg}}" alt=""></a>
             <div class="slider_article">
-              <h2><a class="slider_tittle" href="pages/single_page.html">Fusce eu nulla semper porttitor felis sit amet</a></h2>
+              <h2><a class="slider_tittle" href="pages/single_page.html">{{$post->title}}</a></h2>
               <p>Nunc tincidunt, elit non cursus euismod, lacus augue ornare metus, egestas imperdiet nulla nisl quis mauris. Suspendisse a pharetra urna. Morbi dui...</p>
             </div>
           </div>
-          <div class="single_iteam"> <a href="pages/single_page.html"> <img src="{{asset('user_asset/assets/images/logo.jpg')}}" alt=""></a>
-            <div class="slider_article">
-              <h2><a class="slider_tittle" href="pages/single_page.html">Fusce eu nulla semper porttitor felis sit amet</a></h2>
-              <p>Nunc tincidunt, elit non cursus euismod, lacus augue ornare metus, egestas imperdiet nulla nisl quis mauris. Suspendisse a pharetra urna. Morbi dui...</p>
-            </div>
-          </div>
-          <div class="single_iteam"> <a href="pages/single_page.html"> <img src="{{asset('user_asset/assets/images/logo.jpg')}}" alt=""></a>
-            <div class="slider_article">
-              <h2><a class="slider_tittle" href="pages/single_page.html">Fusce eu nulla semper porttitor felis sit amet</a></h2>
-              <p>Nunc tincidunt, elit non cursus euismod, lacus augue ornare metus, egestas imperdiet nulla nisl quis mauris. Suspendisse a pharetra urna. Morbi dui...</p>
-            </div>
-          </div>
-          <div class="single_iteam"> <a href="pages/single_page.html"> <img src="{{asset('user_asset/assets/images/logo.jpg')}}" alt=""></a>
-            <div class="slider_article">
-              <h2><a class="slider_tittle" href="pages/single_page.html">Fusce eu nulla semper porttitor felis sit amet</a></h2>
-              <p>Nunc tincidunt, elit non cursus euismod, lacus augue ornare metus, egestas imperdiet nulla nisl quis mauris. Suspendisse a pharetra urna. Morbi dui...</p>
-            </div>
-          </div>
+          @endforeach
         </div>
       </div>
