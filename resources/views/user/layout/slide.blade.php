acbac30f58f5f183331a3b4f73d8f5ff451dd29b
Tuan Nguyen <tuanna.it96@gmail.com>
2021-01-03 08:58:47 +0700

    href slide

--- resources/views/user/layout/slide.blade.php
+++ resources/views/user/layout/slide.blade.php
@@ -1,10 +1,10 @@
 <div class="col-lg-8 col-md-8 col-sm-8">
         <div class="slick_slider">
           @foreach($postslide as $post)
-          <div class="single_iteam"> <a href="pages/single_page.html"> <img src="{{$post->thumbimg}}" alt=""></a>
+          <div class="single_iteam"> <a href="{{route('show.post',$post->id)}}"> <img src="{{$post->thumbimg}}" alt=""></a>
             <div class="slider_article">
-              <h2><a class="slider_tittle" href="pages/single_page.html">{{$post->title}}</a></h2>
-              <p>Nunc tincidunt, elit non cursus euismod, lacus augue ornare metus, egestas imperdiet nulla nisl quis mauris. Suspendisse a pharetra urna. Morbi dui...</p>
+              <h2><a class="slider_tittle" href="{{route('show.post',$post->id)}}">{{$post->title}}</a></h2>
+              <p>{{substr(strip_tags( $post->content ),0,150)  }}</p>
             </div>
           </div>
           @endforeach
