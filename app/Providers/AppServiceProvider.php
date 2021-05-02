eb02f7bf60d2bd439eab5b5f324118a949fa1fa1
NGUYEN THACH <peternguyen0293@gmail.com>
2021-05-02 09:58:57 +0700

    fix bien toan cuc trong AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Notifications;
+use View;
+use App\Post;
+use App\Document;
+use App\Link;
+use App\Category;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +28,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(Post::all()->where('status',1)->count() > 5)
+        {
+            $postslide = Post::all()->where('status',1)->sortByDesc('created_at')->take(5);
+            $lstpopularpost = Post::all()->where('status',1)->random(5);
+        }
+        else
+        {
+            $postslide = Post::all()->where('status',1);
+            $lstpopularpost = $postslide;
+        }
+        $lstnoti = Notifications::all()->where('status',1)->sortByDesc('created_at');
+        $link = Link::all()->where('status', 1);
+        $document = Document::all();
+        $lstcat = Category::all()->where('status', 1);
+        View::share('lstnoti', $lstnoti);
+        View::share('postslide',$postslide);
+        View::share('lstpopularpost',$lstpopularpost);
+        View::share('link', $link);
+        View::share('document', $document);
+        View::share('lstcat', $lstcat);
+
     }
 }
